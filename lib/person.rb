8d2c281c2a70c26902aa0b2bb1d1d68be1506b04
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-25 10:21:26 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def talk
+    puts "Hello World!"
+  end 
+  
+  def walk
+    puts "The Person is walking"
+  end
+  
+  
+  
+  
+  
+end
