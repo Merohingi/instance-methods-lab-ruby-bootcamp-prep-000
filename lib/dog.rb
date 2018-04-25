2481877b202997461af2770fbb7e08d49e94553c
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-25 10:20:20 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def bark
+    puts "wolf"
+  end
+  
+  def sit
+    puts "The Dog is sitting"
+  end
+  
+end
+  
+  
+  
+  
+  
+end
